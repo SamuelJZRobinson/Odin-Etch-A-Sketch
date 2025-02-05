5538354e497ae05790e51a90120739f67ada7986
Samuel Robinson <samueljzrobinson@gmail.com>
2025-02-06 00:57:34 +1000

    Removed Unecessary Flex Container, Added Code Documentation

--- js/core.js
+++ js/core.js
@@ -1,7 +1,7 @@
-// Set Main Container
+// Main Container
 const MAIN_CONTAINER = document.querySelector("#main-container");
 
-// Set Canvas
+// Canvas
 const CANVAS_SIZE = 600;
 let gridSize = 16;
 const CELL_BORDER_SIZE = 1;
@@ -12,6 +12,10 @@ let selectedColour = null;
 const CANVAS = document.querySelector("#canvas");
 MAIN_CONTAINER.style.width = CANVAS.style.width = CANVAS.style.height = `${CANVAS_SIZE}px`;
 
+/**
+ * Generates a grid of cells in the canvas.
+ * Each cell listens to mouse events and sets the background colour.
+ */
 function createGrid(){
   const TOTAL_CELLS = gridSize * gridSize;
   const CELL_SIZE = (CANVAS_SIZE/gridSize) - (CELL_BORDER_SIZE*2);
@@ -31,8 +35,12 @@ function createGrid(){
 
   console.log("Created grid");
 }
+// Default canvas grid
 createGrid();
 
+/**
+ * Deletes all cells in the canvas.
+ */
 function clearGrid(){
   while (CANVAS.firstChild) {
     CANVAS.removeChild(CANVAS.firstChild);
@@ -41,8 +49,13 @@ function clearGrid(){
   console.log("Cleared grid");
 }
 
+/**
+ * Sets the background colours of cells based on the selected tool and selected colour.
+ * A temporary colour variable is set to preserve the selected colour when using the eraser.
+ * 
+ * @param {*} e - Triggered mouse event.
+ */
 function setBgColour(e){
-  // Use seperate colour variable to preserve selected colour
   let colorToApply = (selectedTool === "draw") ? selectedColour : "white";
 
   // Mouse Event
@@ -56,19 +69,28 @@ function setBgColour(e){
   }
 }
 
-// Set Options
+// Settings
 const BUT_DRAW = document.querySelector("#but-draw");
 const BUT_ERASE = document.querySelector("#but-erase");
 
-function setDefaultToolBgColour(){
+/**
+ * Sets all tool buttons to a default state to prepare for another selection.
+ * Purely cosmetic and ensures only the selected tool is highlighted.
+ */
+function deactivateTools(){
   BUT_DRAW.classList.remove("active");
   BUT_ERASE.classList.remove("active");
 }
 
+/**
+ * Sets the active tool that interacts with the canvas.
+ * 
+ * @param {*} toolName - The name of the tool being selected that is given an active class.
+ */
 function setTool(toolName){
   selectedTool = toolName;
 
-  setDefaultToolBgColour();
+  deactivateTools();
 
   if (selectedTool === "draw") {
     BUT_DRAW.classList.add("active");
@@ -76,6 +98,7 @@ function setTool(toolName){
     BUT_ERASE.classList.add("active");
   }
 }
+// Default tool
 setTool("draw");
 
 const SLIDER_CONTAINER = document.querySelector("#slider-container");
@@ -84,6 +107,9 @@ const SLIDER_VALUE = document.querySelector("#grid-size-value");
 SLIDER_VALUE.textContent = `Size: ${SLIDER.value} x ${SLIDER.value}`;
 SLIDER.addEventListener("change", (e) => setGridSize());
 
+/**
+ * Set the quantity of grid cells generated after a change is made to the slider.
+ */
 function setGridSize(){
   SLIDER.innerHTML = SLIDER_VALUE.textContent = `Size: ${SLIDER.value} x ${SLIDER.value}`;
   gridSize = SLIDER.value
@@ -91,11 +117,21 @@ function setGridSize(){
   createGrid();
 }
 
+/**
+ * Dynamically change slider grid size values independent of grid updates.
+ */
 SLIDER.oninput = function(){
   SLIDER.innerHTML = SLIDER_VALUE.textContent = `Size: ${this.value} x ${this.value}`;
 }
 
+/**
+ * Set the active colour applied to the canvas.
+ * Doesn't apply to the eraser tool.
+ * 
+ * @param {*} colourName - The name of the colour being selected that is applied to the canvas using the drawing tool.
+ */
 function setColour(colourName){
   selectedColour = colourName;
 }
+// Default colour
 setColour("black");
